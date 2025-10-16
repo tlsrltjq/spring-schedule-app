e26848a3a8179d2f1eba9229b555b0b687a252c8
tlsrltjq <psl87531@naver.com>
2025-10-16 19:56:10 +0900

    initialize Spring Boot project

---
+++ src/main/java/com/example/springscheduleapp/SpringScheduleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscheduleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheduleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScheduleAppApplication.class, args);
+    }
+
+}
